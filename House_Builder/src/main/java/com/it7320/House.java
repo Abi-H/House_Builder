99c5d8bf71b5803d404b5170028849d3d15e7077
abi <abiga@Galahad>
2018-10-09 10:03:32 +1300

    Initial commit

---
+++ House_Builder/src/main/java/com/it7320/House.java
@@ -0,0 +1,79 @@
+package com.it7320;
+
+public class House {
+	int pricePerSquareMetre, squareMetres, bedrooms, bathrooms, priceBedroom, priceBathroom, totalPrice;
+	String houseType;
+
+	public House(int squareMetres, String houseType) {
+		setHouseType(houseType);
+		setSquareMetres(squareMetres);
+		setPricePerSquareMetre(getHouseType());
+		setBedrooms(0);
+		setBathrooms(0);
+		this.priceBedroom = 25000;
+		this.priceBathroom = 20000;
+	}
+
+	public void setPricePerSquareMetre(String type) {
+		if (type.equals("deluxe")) {
+			this.pricePerSquareMetre = 1000;
+		} else if (type.equals("custom")) {
+			this.pricePerSquareMetre = 1500;
+		} else if (type.equals("deluxe custom")) {
+			this.pricePerSquareMetre = 2500;
+		} else {
+			setHouseType("basic");
+			this.pricePerSquareMetre = 620;
+		}
+	}
+	
+	public boolean withinBudget(int budget) {
+		return getTotalPrice() <= budget;
+	}
+
+	public int getTotalPrice() {
+		return (getPricePerSquareMetre() * getSquareMetres()) + (getBedrooms() * priceBedroom) + (getBathrooms() * priceBathroom);
+	}
+	
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getHouseType() {
+		return houseType;
+	}
+
+	public void setHouseType(String houseType) {
+		this.houseType = houseType;
+	}
+
+	public int getSquareMetres() {
+		return squareMetres;
+	}
+
+	public void setSquareMetres(int squareMetres) {
+		this.squareMetres = squareMetres;
+	}
+	
+	public int getBedrooms() {
+		return bedrooms;
+	}
+
+	public void setBedrooms(int bedrooms) {
+		this.bedrooms = bedrooms;
+	}
+
+	public int getBathrooms() {
+		return bathrooms;
+	}
+
+	public void setBathrooms(int bathrooms) {
+		this.bathrooms = bathrooms;
+	}
+
+	public int getPricePerSquareMetre() {
+		return pricePerSquareMetre;
+	}
+
+
+}
---
+++ House_Builder/src/test/java/com/it7320/HouseTest.java
@@ -0,0 +1,253 @@
+package com.it7320;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.it7320.House;
+
+public class HouseTest {
+
+
+	House basicHouse, customHouse, deluxeHouse, deluxeCustomHouse;
+	int basicSize, deluxeSize, customSize, deluxeCustomSize, smallBudget, modestBudget, largeBudget, maxBudget;
+	int smallBedrooms, smallBathrooms, mediumBedrooms, mediumBathrooms, largeBedrooms, largeBathrooms;
+
+	@Before
+	public void setUp() throws Exception {
+		basicSize = 160;
+		deluxeSize = 240;
+		customSize = 320;
+		deluxeCustomSize = 400;
+		
+		smallBudget = 200000;
+		modestBudget = 350000;
+		largeBudget = 600000;
+		maxBudget = 1200000;
+		
+		smallBedrooms = 1;
+		mediumBedrooms = 3;
+		largeBedrooms = 4;
+		
+		smallBathrooms = 1;
+		mediumBathrooms = 1;
+		largeBathrooms = 2;
+
+		basicHouse = new House(basicSize, "basic");
+		deluxeHouse = new House(deluxeSize, "deluxe");
+		customHouse = new House(customSize, "custom");
+		deluxeCustomHouse = new House(deluxeCustomSize, "deluxe custom");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		basicHouse = null;
+		deluxeHouse = null;
+		customHouse = null;
+		deluxeCustomHouse = null;
+	}
+
+	@Test
+	public void testWithinSmallBudget() {		
+		basicHouse.setBedrooms(smallBedrooms);
+		basicHouse.setBathrooms(smallBathrooms);
+		assertTrue(basicHouse.withinBudget(smallBudget));
+				
+		basicHouse.setBedrooms(mediumBedrooms);
+		basicHouse.setBathrooms(mediumBathrooms);
+		assertTrue(basicHouse.withinBudget(smallBudget));
+		
+		basicHouse.setBedrooms(largeBedrooms);
+		basicHouse.setBathrooms(largeBathrooms);
+		assertFalse(basicHouse.withinBudget(smallBudget));
+		
+		deluxeHouse.setBedrooms(smallBedrooms);
+		deluxeHouse.setBathrooms(smallBathrooms);
+		assertFalse(deluxeHouse.withinBudget(smallBudget));
+		
+		deluxeHouse.setBedrooms(mediumBedrooms);
+		deluxeHouse.setBathrooms(mediumBathrooms);
+		assertFalse(deluxeHouse.withinBudget(smallBudget));
+		
+		deluxeHouse.setBedrooms(largeBedrooms);
+		deluxeHouse.setBathrooms(largeBathrooms);
+		assertFalse(deluxeHouse.withinBudget(smallBudget));
+		
+		customHouse.setBedrooms(smallBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertFalse(customHouse.withinBudget(smallBudget));
+		
+		customHouse.setBedrooms(mediumBedrooms);
+		customHouse.setBathrooms(mediumBathrooms);
+		assertFalse(customHouse.withinBudget(smallBudget));
+		
+		customHouse.setBedrooms(largeBedrooms);
+		customHouse.setBathrooms(largeBathrooms);
+		assertFalse(customHouse.withinBudget(smallBudget));
+		
+		deluxeCustomHouse.setBedrooms(mediumBedrooms);
+		deluxeCustomHouse.setBathrooms(mediumBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));
+		
+		deluxeCustomHouse.setBedrooms(largeBedrooms);
+		deluxeCustomHouse.setBathrooms(largeBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));	
+	}
+
+	@Test
+	public void testWithinModestBudget() {
+		basicHouse.setBedrooms(smallBedrooms);
+		basicHouse.setBathrooms(smallBathrooms);
+		assertTrue(basicHouse.withinBudget(modestBudget));
+				
+		basicHouse.setBedrooms(mediumBedrooms);
+		basicHouse.setBathrooms(mediumBathrooms);
+		assertTrue(basicHouse.withinBudget(modestBudget));
+		
+		basicHouse.setBedrooms(largeBedrooms);
+		basicHouse.setBathrooms(largeBathrooms);
+		assertTrue(basicHouse.withinBudget(modestBudget));
+		
+		deluxeHouse.setBedrooms(smallBedrooms);
+		deluxeHouse.setBathrooms(smallBathrooms);
+		assertTrue(deluxeHouse.withinBudget(modestBudget));
+		
+		deluxeHouse.setBedrooms(mediumBedrooms);
+		deluxeHouse.setBathrooms(mediumBathrooms);
+		assertTrue(deluxeHouse.withinBudget(modestBudget));
+		
+		deluxeHouse.setBedrooms(largeBedrooms);
+		deluxeHouse.setBathrooms(largeBathrooms);
+		assertFalse(deluxeHouse.withinBudget(modestBudget));
+		
+		customHouse.setBedrooms(smallBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertFalse(customHouse.withinBudget(modestBudget));
+		
+		customHouse.setBedrooms(mediumBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertFalse(customHouse.withinBudget(modestBudget));
+		
+		customHouse.setBedrooms(largeBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertFalse(customHouse.withinBudget(modestBudget));
+		
+		deluxeCustomHouse.setBedrooms(smallBedrooms);
+		deluxeCustomHouse.setBathrooms(smallBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(modestBudget));
+		
+		deluxeCustomHouse.setBedrooms(mediumBedrooms);
+		deluxeCustomHouse.setBathrooms(mediumBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(modestBudget));
+		
+		deluxeCustomHouse.setBedrooms(largeBedrooms);
+		deluxeCustomHouse.setBathrooms(largeBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(modestBudget));
+	}
+
+	@Test
+	public void testWithinLargeBudget() {
+		basicHouse.setBedrooms(smallBedrooms);
+		basicHouse.setBathrooms(smallBathrooms);
+		assertTrue(basicHouse.withinBudget(largeBudget));
+				
+		basicHouse.setBedrooms(mediumBedrooms);
+		basicHouse.setBathrooms(mediumBathrooms);
+		assertTrue(basicHouse.withinBudget(largeBudget));
+		
+		basicHouse.setBedrooms(largeBedrooms);
+		basicHouse.setBathrooms(largeBathrooms);
+		assertTrue(basicHouse.withinBudget(largeBudget));
+		
+		deluxeHouse.setBedrooms(smallBedrooms);
+		deluxeHouse.setBathrooms(smallBathrooms);
+		assertTrue(deluxeHouse.withinBudget(largeBudget));
+		
+		deluxeHouse.setBedrooms(mediumBedrooms);
+		deluxeHouse.setBathrooms(mediumBathrooms);
+		assertTrue(deluxeHouse.withinBudget(largeBudget));
+		
+		deluxeHouse.setBedrooms(largeBedrooms);
+		deluxeHouse.setBathrooms(largeBathrooms);
+		assertTrue(deluxeHouse.withinBudget(largeBudget));
+		
+		customHouse.setBedrooms(smallBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(largeBudget));
+		
+		customHouse.setBedrooms(mediumBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(largeBudget));
+		
+		customHouse.setBedrooms(largeBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(largeBudget));
+		
+		deluxeCustomHouse.setBedrooms(smallBedrooms);
+		deluxeCustomHouse.setBathrooms(smallBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));
+		
+		deluxeCustomHouse.setBedrooms(mediumBedrooms);
+		deluxeCustomHouse.setBathrooms(mediumBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));
+		
+		deluxeCustomHouse.setBedrooms(largeBedrooms);
+		deluxeCustomHouse.setBathrooms(largeBathrooms);
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));
+	}
+	
+	@Test
+	public void testWithinMaxBudget() {
+		basicHouse.setBedrooms(smallBedrooms);
+		basicHouse.setBathrooms(smallBathrooms);
+		assertTrue(basicHouse.withinBudget(maxBudget));
+				
+		basicHouse.setBedrooms(mediumBedrooms);
+		basicHouse.setBathrooms(mediumBathrooms);
+		assertTrue(basicHouse.withinBudget(maxBudget));
+		
+		basicHouse.setBedrooms(largeBedrooms);
+		basicHouse.setBathrooms(largeBathrooms);
+		assertTrue(basicHouse.withinBudget(maxBudget));
+		
+		deluxeHouse.setBedrooms(smallBedrooms);
+		deluxeHouse.setBathrooms(smallBathrooms);
+		assertTrue(deluxeHouse.withinBudget(maxBudget));
+		
+		deluxeHouse.setBedrooms(mediumBedrooms);
+		deluxeHouse.setBathrooms(mediumBathrooms);
+		assertTrue(deluxeHouse.withinBudget(maxBudget));
+		
+		deluxeHouse.setBedrooms(largeBedrooms);
+		deluxeHouse.setBathrooms(largeBathrooms);
+		assertTrue(deluxeHouse.withinBudget(maxBudget));
+		
+		customHouse.setBedrooms(smallBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(maxBudget));
+		
+		customHouse.setBedrooms(mediumBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(maxBudget));
+		
+		customHouse.setBedrooms(largeBedrooms);
+		customHouse.setBathrooms(smallBathrooms);
+		assertTrue(customHouse.withinBudget(maxBudget));
+		
+		deluxeCustomHouse.setBedrooms(smallBedrooms);
+		deluxeCustomHouse.setBathrooms(smallBathrooms);
+		assertTrue(deluxeCustomHouse.withinBudget(maxBudget));
+		
+		deluxeCustomHouse.setBedrooms(mediumBedrooms);
+		deluxeCustomHouse.setBathrooms(mediumBathrooms);
+		assertTrue(deluxeCustomHouse.withinBudget(maxBudget));
+		
+		deluxeCustomHouse.setBedrooms(largeBedrooms);
+		deluxeCustomHouse.setBathrooms(largeBathrooms);
+		assertTrue(deluxeCustomHouse.withinBudget(maxBudget));
+	}
+
+}
+
